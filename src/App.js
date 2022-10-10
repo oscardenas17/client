1d1f6b00a86f3f965214e8d16916da72c87663b6
oscardenas17 <oscardenas17@gmail.com>
2022-10-10 08:58:37 -0500

    prueba jwt refresh token

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const refreshToken = async () => {
+    try {
+      const res = await axios.post("http://localhost:8000/api/refresh", {
+        token: user.refreshToken,
+      });
+      setUser({
+        ...user,
+        accessToken: res.data.accessToken,
+        refreshToken: res.data.refreshToken,
+      });
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  /* Creating a new instance of axios. */
+  const axiosJWT = axios.create();
+  /* This is an interceptor that checks if the access token is expired. If it is, it will refresh the
+  token and add it to the header. */
+  axiosJWT.interceptors.request.use(
+    async (config) => {
+      let currentDate = new Date();
+      const decodedToken = jwt_decode(user.accessToken);
+      if (decodedToken.exp * 1000 < currentDate.getTime()) {
+        const data = await refreshToken();
+        config.headers["authorization"] = "Bearer " + data.accessToken;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    try {
+      const res = await axios.post("http://localhost:8000/api/login", {
+        username,
+        password,
+      });
+      setUser(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    setSuccess(false);
+    setError(false);
+    try {
+      //await axios.delete("http://localhost:8000/api/users/" + id, {
+      await axiosJWT.delete("http://localhost:8000/api/users/" + id, {
+        headers: { authorization: "Bearer " + user.accessToken },
+      });
+      setSuccess(true);
+    } catch (err) {
+      setError(true);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {user ? (
+        <div className="home">
+          <span>
+            Hola <b>{user.isAdmin ? "admin" : "user"}</b>¡{" "}
+            <b>{user.userName}</b>.
+          </span>
+          <span>Usuarios a eliminar:</span>
+          <button className="deleteButton" onClick={() => handleDelete(1)}>
+            Delete John
+          </button>
+          <button className="deleteButton" onClick={() => handleDelete(2)}>
+            Delete Jane
+          </button>
+          {error && (
+            <span className="error">Sin permiso para eliminar el usuario</span>
+          )}
+          {success && <span className="success">Usuario Eliminado..</span>}
+        </div>
+      ) : (
+        <div className="login">
+          <form onSubmit={handleSubmit}>
+            <span className="formTitle">Login</span>
+            <input
+              type="text"
+              placeholder="username"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit" className="submitButton">
+              Login
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
